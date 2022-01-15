77284ec91eea6e8ecef2bf3f4d5e9292e373a57c
Washu-Misaki <92064229+Washu-Misaki@users.noreply.github.com>
2022-01-14 21:03:49 -0700

    Update dom-basics.js

--- dom-basics.js
+++ dom-basics.js
@@ -12,5 +12,5 @@ newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
 document.body.appendChild(newDiv);
 
 const newSection = document.createElement("section");
-newSection.innerHTML = "<h2>CSE 121b"</h2><p>Welcome to Javascript Language</p>";
+newSection.innerHTML = "<h2>CSE 121b</h2><p>Welcome to Javascript Language</p>";
 document.body.appendChild(newSection);
