c698e4df5ef5ce456622e0580df99f9e73894752
Washu-Misaki <92064229+Washu-Misaki@users.noreply.github.com>
2022-01-14 20:51:15 -0700

    Update dom-basics.js

--- dom-basics.js
+++ dom-basics.js
@@ -6,3 +6,11 @@ const newImage = document.createElement("img");
 newImage.setAttribute("src", "https://placeimg.com/200/200/animals");
 newImage.setAttribute("alt", "Description of image");
 document.body.appendChild(newImage);
+
+const NewDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>CSE 121b"</h2><p>Welcome to Javascript Language</p>";
+document.body.appendChild(newSection);
